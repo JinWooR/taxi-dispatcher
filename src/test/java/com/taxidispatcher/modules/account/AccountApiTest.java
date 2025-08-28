bc1ec5e400b29d749bb4912e2660b6339f0444ea
kingtest <kingtest1024@gmail.com>
2025-08-28 18:06:32 +0900

    test: 어카운트 테스트 코드 작성

---
+++ src/test/java/com/taxidispatcher/modules/account/AccountApiTest.java
@@ -0,0 +1,65 @@
+package com.taxidispatcher.modules.account;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*; // get, post, put...
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.taxidispatcher.modules.account.adapter.web.dto.request.PasswordLoginRequest;
+import com.taxidispatcher.modules.account.adapter.web.dto.request.RegisterBasicRequest;
+import com.taxidispatcher.modules.account.adapter.web.dto.response.LoginResponse;
+import com.taxidispatcher.modules.account.domain.model.IdentifierKind;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AccountApiTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private final String loginId = "tester_001";
+    private final String password = "test012!";
+
+    @Test
+    void fullTest() throws Exception {
+        // 어카운트 등록
+        RegisterBasicRequest registerBasicRequest = new RegisterBasicRequest(
+                IdentifierKind.ID,
+                loginId,
+                password
+        );
+
+        mockMvc
+                .perform(
+                        post("/auth/register")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(registerBasicRequest))
+                )
+                .andExpect(status().isCreated());
+
+        // 로그인
+        PasswordLoginRequest loginRequest = new PasswordLoginRequest(
+                loginId, password
+        );
+        var loginRes = mockMvc
+                .perform(
+                        post("/auth/login")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(loginRequest))
+                )
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String loginToken = null;
+        if (loginRes.getResponse().getStatus() >= 200 && loginRes.getResponse().getStatus() < 300) {
+            loginToken = objectMapper.readValue(loginRes.getResponse().getContentAsString(), LoginResponse.class)
+                    .token();
+        }
+    }
+}

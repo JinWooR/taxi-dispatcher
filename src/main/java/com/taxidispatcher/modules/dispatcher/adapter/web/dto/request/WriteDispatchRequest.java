c5104e28cde6e65ed1822a19f533787b43d98b20
kingtest <kingtest1024@gmail.com>
2025-09-05 17:29:05 +0900

    fix: 배차 요청서 위경도 변수 타입 변경3 (String -> Double)

--- src/main/java/com/taxidispatcher/modules/dispatcher/adapter/web/dto/request/WriteDispatchRequest.java
+++ src/main/java/com/taxidispatcher/modules/dispatcher/adapter/web/dto/request/WriteDispatchRequest.java
@@ -2,15 +2,18 @@ package com.taxidispatcher.modules.dispatcher.adapter.web.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
 import org.springframework.validation.annotation.Validated;
 
 public record WriteDispatchRequest(
         @NotNull @Validated AddressGeo startAddress,
         @NotNull @Validated AddressGeo arrivalAddress
 ) {
+
+    @Getter
     public static class AddressGeo {
         @NotBlank String address;
-        @NotBlank String x;
-        @NotBlank String y;
+        @NotNull Double x;
+        @NotNull Double y;
     }
 }

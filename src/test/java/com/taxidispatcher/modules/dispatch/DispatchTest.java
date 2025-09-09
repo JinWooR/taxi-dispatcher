4ba9e1f997134bedc63a8fe6bdff14654ce07162
kingtest <kingtest1024@gmail.com>
2025-09-09 20:45:45 +0900

    test: 배차 테스트 헬퍼 및 테스트 코드 추가 (기존 테스트 코드 제거)

---
+++ src/test/java/com/taxidispatcher/modules/dispatch/DispatchTest.java
@@ -0,0 +1,50 @@
+package com.taxidispatcher.modules.dispatch;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.taxidispatcher.core.TestBase;
+import com.taxidispatcher.modules.account.AccountApiHelper;
+import com.taxidispatcher.modules.dispatcher.adapter.web.dto.request.WriteDispatchRequest;
+import com.taxidispatcher.modules.user.UserApiHelper;
+import com.taxidispatcher.modules.user.adapter.web.dto.request.RegisterUserRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+
+public class DispatchTest extends TestBase {
+    private final AccountApiHelper accountApiHelper;
+    private final UserApiHelper userApiHelper;
+    private final DispatchApiHelper dispatchApiHelper;
+
+    public DispatchTest(@Autowired MockMvc mockMvc, @Autowired ObjectMapper objectMapper) {
+        super(mockMvc, objectMapper);
+        this.accountApiHelper = new AccountApiHelper(mockMvc, objectMapper, "test001", "test012!");
+        this.userApiHelper = new UserApiHelper(mockMvc, objectMapper);
+        this.dispatchApiHelper = new DispatchApiHelper(mockMvc, objectMapper);
+    }
+
+    @Test
+    void fullTest() throws Exception {
+        accountApiHelper.register();
+        String token = "Bearer " + accountApiHelper.login();
+
+        userApiHelper.setToken(token);
+        userApiHelper.register(new RegisterUserRequest("테스터", "서울 강남구 가로수길 9", "13층"));
+
+        token = "Bearer " + accountApiHelper.loginUser();
+
+        dispatchApiHelper.setToken(token);
+        var dispatch = dispatchApiHelper.write(
+                new WriteDispatchRequest(
+                        new WriteDispatchRequest.AddressGeo("ex)출발지", 37.5705d, 126.9774d),
+                        new WriteDispatchRequest.AddressGeo("ex)목적지", 37.5490d, 127.0812d)
+                )
+        );
+
+        if (dispatch.isPresent()) {
+            // 배차 취소
+            dispatchApiHelper.cancel(dispatch.get().dispatchId().toString());
+        } else {
+            throw new Exception("배차 요청 응답 수신 오류");
+        }
+    }
+}

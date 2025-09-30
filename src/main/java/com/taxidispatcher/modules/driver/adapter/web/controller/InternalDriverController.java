47ea33de3b9d068403e11ead58193142aa652ec3
kingtest <kingtest1024@gmail.com>
2025-09-30 21:30:58 +0900

    feat: 기사 (내부용) 상태 변경 API 추가

--- src/main/java/com/taxidispatcher/modules/driver/adapter/web/controller/InternalDriverController.java
+++ src/main/java/com/taxidispatcher/modules/driver/adapter/web/controller/InternalDriverController.java
@@ -1,10 +1,15 @@
 package com.taxidispatcher.modules.driver.adapter.web.controller;
 
 import com.taxidispatcher.modules.driver.adapter.web.dto.request.InternalDriverNearbyGeoRequest;
+import com.taxidispatcher.modules.driver.adapter.web.dto.request.UpdateDriverActiveStatusRequest;
+import com.taxidispatcher.modules.driver.adapter.web.dto.response.DriverResponse;
 import com.taxidispatcher.modules.driver.adapter.web.dto.response.InternalDriverAccountResponse;
 import com.taxidispatcher.modules.driver.adapter.web.dto.response.InternalDriverResponse;
 import com.taxidispatcher.modules.driver.application.port.in.*;
+import com.taxidispatcher.modules.driver.domain.aggregate.Driver;
+import com.taxidispatcher.modules.driver.domain.model.DriverGeo;
 import com.taxidispatcher.modules.driver.domain.model.DriverId;
+import com.taxidispatcher.modules.driver.domain.model.Taxi;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +25,7 @@ public class InternalDriverController {
     private final InternalSearchDriverAccountUseCase internalSearchDriverAccountUseCase;
     private final InternalSearchDriverIdUseCase internalSearchDriverIdUseCase;
     private final InternalSearchDriverNearbyGeoAdapter internalSearchDriverNearbyGeoAdapter;
+    private final UpdateDriverActiveStatusUseCase updateDriverActiveStatusUseCase;
 
     @GetMapping("account/{id}")
     public ResponseEntity<InternalDriverAccountResponse> accountId(@PathVariable(name = "id") String accountId) {
@@ -39,5 +45,24 @@ public class InternalDriverController {
     ) {
         return ResponseEntity
                 .ok(internalSearchDriverNearbyGeoAdapter.handle(new InternalSearchDriverNearbyGeoCommand(request.driverIds().stream().map(DriverId::new).toList(), request.x(), request.y(), request.distance())));
+    }
+
+    // 택시 상태 변경
+    @PostMapping("{driverId}/active-status")
+    public ResponseEntity<DriverResponse> changeActiveStatus(
+            @PathVariable String driverId,
+            @Valid @RequestBody UpdateDriverActiveStatusRequest request
+    ) {
+        Driver driver = updateDriverActiveStatusUseCase.handle(new UpdateDriverActiveStatusCommand(DriverId.strId(driverId), request.activeStatus()));
+
+        return ResponseEntity
+                .ok(toRes(driver));
+    }
+
+    private DriverResponse toRes(Driver driver) {
+        DriverId driverId = driver.getId();
+        DriverGeo curGeo = driver.getCurGeo();
+        Taxi taxi = driver.getTaxi();
+        return new DriverResponse(driverId.id(), driver.getStatus(), driver.getName(), curGeo.lat(), curGeo.lng(), taxi.taxiNumber(), taxi.size(), taxi.color(), taxi.otherColor(), driver.getActiveStatus());
     }
 }

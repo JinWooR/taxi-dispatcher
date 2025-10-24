42452ab8ffaf4c612ed512c9b049861af7866037
kingtest <kingtest1024@gmail.com>
2025-10-24 23:15:25 +0900

    fix(0b22c8b0): 배차) 배차 운행 좌표 정보 도메인 아이디 수정

--- src/main/java/com/taxidispatcher/modules/dispatcher/domain/model/DispatchDriverGeoHistoryId.java
+++ src/main/java/com/taxidispatcher/modules/dispatcher/domain/model/DispatchDriverGeoHistoryId.java
@@ -1,6 +1,4 @@
 package com.taxidispatcher.modules.dispatcher.domain.model;
 
-import java.util.UUID;
-
-public record DispatchDriverGeoHistoryId(UUID driverId, Long seq) {
+public record DispatchDriverGeoHistoryId(DispatchId dispatchId, Long seq) {
 }
